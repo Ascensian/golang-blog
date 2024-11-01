0edb0840f938bd302e0ca879a0e4ce5d6139b634
Hugo GERARD <hg3bd20l>
2024-11-01 23:35:09 +0100

    add database connection

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"example.com/mod/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	database.Connect()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	app := fiber.New()
+	app.Listen(":" + port)
 }
